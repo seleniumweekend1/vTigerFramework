44f14be0a04f5dfd13ef68c6506015ca3843401d
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-05-04 08:12:06 +0530

    Adding on test script - NewTestScript1

---
+++ vTigerCRM/src/com/vTigerCRM/testScripts/NewTestScript1.java
@@ -0,0 +1,12 @@
+package com.vTigerCRM.testScripts;
+
+import org.testng.annotations.Test;
+
+public class NewTestScript1 
+{
+	@Test
+	public void newTestScript1()
+	{
+		System.out.println("NewTestScript1");
+	}
+}
