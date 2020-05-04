27bba9f5c75ba628eb4f3625475a02c58030d33c
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-05-04 08:28:48 +0530

    New TS

---
+++ vTigerCRM/src/com/vTigerCRM/testScripts/NewTestScript2.java
@@ -0,0 +1,14 @@
+package com.vTigerCRM.testScripts;
+
+import org.testng.annotations.Test;
+
+import com.vTigerCRM.genericLib.Base;
+
+public class NewTestScript2
+{
+	@Test
+	public void newTestScript2()
+	{
+		System.out.println("NewTestScript2");
+	}
+}
